981b514761becc0e0c2531a5c1d6600f196baab9
RALAIARIMANGA Harimbola Roland <roland.ralaiarimanga@matic.mg>
2015-01-16 10:35:38 +0300

    Signed-off-by: RALAIARIMANGA Harimbola Roland <roland.ralaiarimanga@matic.mg>

---
+++ matic_l10n_mg_paye/__openerp__.py
@@ -0,0 +1,39 @@
+﻿{
+    'name': 'Madagascar - Gestion de la Paie',
+    'category': 'Localization/Payroll',
+    'author': 'MaTIC - Madagascar Technologies de l\'Information et de la Communication',
+    'website': 'http://www.matic.mg',
+    "category" : "Localization",
+    'version': '1.0',
+    'depends': ['hr_payroll'],
+    
+	
+    'description': """Madagascar Payroll Rules.
+======================
+
+   Gestion de la Paie Tunisienne:    
+    - Gestion des employés.
+    - Gestion des contrats.
+    - Configuration et paramètrage
+            * Les rubriques de paie :primes,indemnités,avantages,déductions,...
+            * Les rubriques cotisable ,imposable , soumise à la prime d'ancienneté  ...
+            * Les cotisations : cotisations salariales et patronales CNAPS,Mutuelle...
+            * Barème de la prime d'ancienneté,cotisations CNAPS ...       
+    - Calcul de paie selon les normes malagasy : calcul automatique de la prime d'ancienneté,heures supplémentaire,cotisations salariales et patronales,...
+    - Gestion des congés  :Calcul automatique des congés non payés à partir du module hr_holidays
+    - Comptabilisation de la paie :  configuration des comptes de credit et de débit
+    - Reporting : les  bulletins de paie,journale de paie ,Ordres de virement ...
+    """,
+    'active': False,
+    'update_xml':['l10n_mg_paye_view.xml'],
+    'data': [
+        'l10n_mg_paye_view.xml',
+        'l10n_mg_paye_data.xml',
+		
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+	
+
+}
---
+++ matic_l10n_mg_paye/l10n_mg_paye.py
@@ -0,0 +1,58 @@
+﻿from openerp.osv import fields, osv
+import openerp.addons.decimal_precision as dp
+
+class hr_contract(osv.osv):
+    _inherit = 'hr.contract'
+
+    _columns = {
+        'categorie': fields.char('Categorie Professionnel', size=64),
+        'echellon': fields.char('Echellon', size=64),
+        'indice': fields.integer('Indice', size=10),
+        'horaire_hebdo': fields.float('Horaire hébdomadaire',digits_compute=dp.get_precision('Payroll')),
+	'payment_mode': fields.selection([('VIREMENT', 'VIREMENT'),('ESPECE', 'ESPECE')], 'Mode de paiement'),
+    }
+hr_contract()
+
+
+
+class res_company(osv.osv):
+    _inherit = 'res.company'
+
+    _columns = {
+        'seuil_irsa': fields.float('Seuil IRSA', digits_compute=dp.get_precision('Payroll')),
+        'taux_irsa': fields.float('Taux IRSA', digits_compute=dp.get_precision('Payroll')),
+        'abat_irsa': fields.float('Abattement IRSA', digits_compute=dp.get_precision('Payroll')),
+        'cotisation_cnaps_patr': fields.float('Cotisation Patronale CNAPS', digits_compute=dp.get_precision('Payroll')),
+        'cotisation_cnaps_emp': fields.float('Cotisation Employé CNAPS', digits_compute=dp.get_precision('Payroll')),
+        'plafond_cnaps': fields.float('Plafond de la Securite Sociale', digits_compute=dp.get_precision('Payroll')),
+        'num_cnaps_patr': fields.char('N° CNAPS', size=64),
+        'cotisation_sante_patr': fields.float('Cotisation Patronale Santé', digits_compute=dp.get_precision('Payroll')),
+        'cotisation_sante_emp': fields.float('Cotisation Employé Santé', digits_compute=dp.get_precision('Payroll')),
+        'org_sante': fields.char('Organisme sanitaire', size=64),
+        'conge_mens': fields.float('Nombre de jour congé mensuel', digits_compute=dp.get_precision('Payroll')),
+    }
+
+res_company()
+
+class hr_employee(osv.osv):
+    _inherit = 'hr.employee'
+	
+	
+    _columns = {
+		'num_cnaps_emp':fields.integer('N° CNAPS', size=10),
+        'num_emp':fields.char('N° Matricule', size=64),
+        'num_cin':fields.char('N° CIN', size=64),
+	}
+hr_employee()
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
